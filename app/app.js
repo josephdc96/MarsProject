5b4e003b4a1246a6ed74dafec3b4c10526e653d7
Joseph Cauble <josephdc96@hotmail.com>
2017-02-04 09:05:54 -0600

    Created initial App.js framework

--- app/app.js
+++ app/app.js
@@ -2,13 +2,34 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+
+    .controller('myApp', [function($scope, $http) {
+        $scope.queryURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/";
+        $scope.manifestURL = "https://api.nasa.gov/mars-photos/api/v1/manifests/";
+        $scope.manifest = {};
+        $scope.apiKey = "QVVpRu8GN1TT6dqz89kn3DQMBXcDL25RtEO2LKr9";
+
+        $scope.selectRover = function(roverName) {
+            if (roverName === 'curiosity' || roverName === 'spirit' || roverName === 'opportunity') {
+                $scope.queryURL = $scope.queryURL + "curiosity/photos";
+                $scope.manifestURL = $scope.manifestURL + "curiosity";
+                $http.get($scope.manifestURL).then(function(response) {
+                    $scope.manifest = response.data;
+                });
+            }
+            else {
+                
+            }
+
+        }
+    }]);
